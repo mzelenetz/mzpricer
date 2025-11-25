3d58cab79daa6455ba7e62e1d1ab058372f39b2f
Michael Zelenetz <michael.zelenetz@gmail.com>
2025-11-25 00:29:06 -0500

    Cleanup: ignore build artifacts

--- mzpricer-core/target/debug/deps/mzpricer_core-b7310c3f6461c792.d
+++
@@ -1,8 +0,0 @@
-/Users/mz/Documents/projects/pricer/mzpricer/mzpricer-core/target/debug/deps/mzpricer_core-b7310c3f6461c792.d: src/lib.rs src/pricer.rs
-
-/Users/mz/Documents/projects/pricer/mzpricer/mzpricer-core/target/debug/deps/libmzpricer_core-b7310c3f6461c792.rlib: src/lib.rs src/pricer.rs
-
-/Users/mz/Documents/projects/pricer/mzpricer/mzpricer-core/target/debug/deps/libmzpricer_core-b7310c3f6461c792.rmeta: src/lib.rs src/pricer.rs
-
-src/lib.rs:
-src/pricer.rs:
--- mzpricer-core/target/debug/deps/pricer_tests-324153c642a7c6f3.d
+++
@@ -1,5 +0,0 @@
-/Users/mz/Documents/projects/pricer/mzpricer/mzpricer-core/target/debug/deps/pricer_tests-324153c642a7c6f3.d: tests/pricer_tests.rs
-
-/Users/mz/Documents/projects/pricer/mzpricer/mzpricer-core/target/debug/deps/pricer_tests-324153c642a7c6f3: tests/pricer_tests.rs
-
-tests/pricer_tests.rs:
